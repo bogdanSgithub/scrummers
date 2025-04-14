d3df5dbe39c2d74ba78f78d7def9b0d45151c868
Bogdan Andrei Feher <2276038@johnabbottcollege.net>
2025-04-14 09:55:39 -0400

    added the AddExpense window

--- Frontend_HomeBudget/AddExpense.xaml.cs
+++ Frontend_HomeBudget/AddExpense.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -6,25 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Frontend_HomeBudget
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for AddExpense.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class AddExpense : Window
     {
-        public MainWindow()
+        public AddExpense()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            AddExpense addExpense = new AddExpense();
-            addExpense.Show();
         }
     }
 }
