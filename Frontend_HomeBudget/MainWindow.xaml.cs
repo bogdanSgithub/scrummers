53b348663af954a99a6e5d8a77b78165c588a89a
Emile Lévesque <2359794@johnabbottcollege.net>
2025-04-15 12:16:21 -0400

    Finished implementing front end without presenter implementation.

--- Frontend_HomeBudget/MainWindow.xaml.cs
+++ Frontend_HomeBudget/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using Microsoft.Win32;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +9,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using System.Diagnostics.Tracing;
+using System.Text.Json;
+
 
 namespace Frontend_HomeBudget
 {
@@ -19,6 +24,50 @@ namespace Frontend_HomeBudget
         public MainWindow()
         {
             InitializeComponent();
+
+            if (File.Exists("../../../info.json"))
+                returningUserBtn.Visibility = Visibility.Visible;
+
+
         }
+
+        private void FileSelect_Clicked(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "DB files (*.db)|*.db";
+
+            if (fileDialog.ShowDialog() == true)
+            {
+                try
+                {
+                    string escapedFilePath = fileDialog.FileName.Replace("\\", "\\\\");
+
+                    File.WriteAllText("../../../info.json", $"{{ \"current\": \"{ escapedFilePath }\" }}");  
+                }
+                catch (Exception ex)  
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                
+            }
+
+        }
+
+        public class DeserializedFileInfo
+        {
+            public string current { get; set; }
+        }
+
+
+
+        private void PreviousFile_Clicked(object sender, RoutedEventArgs e)
+        {
+            string jsonFileContent = File.ReadAllText("../../../info.json");
+
+            DeserializedFileInfo data = JsonSerializer.Deserialize<DeserializedFileInfo>(jsonFileContent);
+
+            MessageBox.Show(data.current);
+        }
+
     }
 }
