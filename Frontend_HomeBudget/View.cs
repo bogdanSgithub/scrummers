02c3c592cb859cb39c5dda70808aac981fe7d4b1
Bogdan Andrei Feher <2276038@johnabbottcollege.net>
2025-04-17 11:11:15 -0400

    modified the project architecture significantly, now new View class that will handle the windows.

---
+++ Frontend_HomeBudget/View.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BudgetPresenter;
+using System.Windows;
+
+namespace Frontend_HomeBudget
+{
+    class View : IView
+    {   
+        public View()
+        {
+            var app = new Application();
+            AddExpense expenseWindow = new AddExpense("uiwqhuiduhid");
+            app.Run(expenseWindow);
+        }
+
+        public void ShowCompletion(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShowError(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
