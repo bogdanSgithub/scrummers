5d1752f48b9436bf078648beb86f5691a55554f2
SkweesheeEmile <2359794@johnabbottcollege.net>
2025-04-29 12:03:54 -0400

    Finished the main UI for the window, view implementation will be done in other branches

---
+++ Frontend_HomeBudget/HomeBudget.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Frontend_HomeBudget
+{
+    /// <summary>
+    /// Logique d'interaction pour HomeBudget.xaml
+    /// </summary>
+    public partial class HomeBudget : Window
+    {
+        public HomeBudget()
+        {
+            InitializeComponent();
+        }
+    }
+}
